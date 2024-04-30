0c7007d266ac6ed2720b293fc5fce26b4e60a56a
Elias Milian <00092822@uca.edu.sv>
2024-04-29 20:30:14 -0600

    Empezamos

---
+++ Practica_taller1/app/src/main/java/com/eam03/practica_taller/UI/Components/InsertDataComponent.kt
@@ -0,0 +1,8 @@
+package com.eam03.practica_taller.UI.Components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun Nombre(){}
---
+++ Practica_taller1/app/src/main/java/com/eam03/practica_taller/UI/Screens/HomeScren.kt
@@ -0,0 +1,2 @@
+package com.eam03.practica_taller.UI.Screens
+
