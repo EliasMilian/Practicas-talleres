c5ea98d2bc2e3bec263caafb944df542bdb67862
Elias Milian <00092822@uca.edu.sv>
2024-04-29 20:43:45 -0600

    Empezamos

--- Practica_taller1/app/src/main/java/com/eam03/practica_taller/UI/Components/InsertDataComponent.kt
+++ Practica_taller1/app/src/main/java/com/eam03/practica_taller/UI/Components/InsertDataComponent.kt
@@ -1,8 +1,18 @@
 package com.eam03.practica_taller.UI.Components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
-@Preview
+@Preview(showSystemUi = true)
 @Composable
-fun Nombre(){}
+
+fun InsertData(){
+
+    Column (modifier = Modifier.fillMaxSize())
+    {
+
+    }
+}
