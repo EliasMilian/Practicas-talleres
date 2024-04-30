2a113eb8d13adb29eea1163e82a5e5e0b9b22e14
V1ctorHUB <104780174+V1ctorHUB@users.noreply.github.com>
2024-04-29 20:59:17 -0600

    add: Object Class

--- Practica_taller1/app/src/main/java/com/eam03/practica_taller/Data/DummyData.kt
+++ Practica_taller1/app/src/main/java/com/eam03/practica_taller/Data/DummyData.kt
@@ -1,3 +1,7 @@
 package com.eam03.practica_taller.Data
 
-pene
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import com.eam03.practica_taller.models.ObjectClass
+
+val objectList: MutableState<MutableList<ObjectClass>> = mutableStateOf(mutableListOf())
---
+++ Practica_taller1/app/src/main/java/com/eam03/practica_taller/models/ObjectClass.kt
@@ -0,0 +1,13 @@
+package com.eam03.practica_taller.models
+
+class ObjectClass(
+    private var Name: String,
+    private var Age: Int
+){
+    fun getName():String {
+        return Name
+    }
+    fun getAge() :Int {
+        return Age
+    }
+}
