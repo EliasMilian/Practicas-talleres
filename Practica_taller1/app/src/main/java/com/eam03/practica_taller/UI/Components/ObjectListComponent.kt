99149ba783d73d60c6ac800c73a41851a64618ec
V1ctorHUB <104780174+V1ctorHUB@users.noreply.github.com>
2024-04-29 21:51:52 -0600

    add: function GetData

--- Practica_taller1/app/src/main/java/com/eam03/practica_taller/UI/Components/ObjectListComponent.kt
+++ Practica_taller1/app/src/main/java/com/eam03/practica_taller/UI/Components/ObjectListComponent.kt
@@ -1,2 +1,29 @@
-package com.eam03.practica_taller.UI.Components
+package com.eam03.practica_taller.ui.Components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun GetData() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "GET DATA FROM DUMMY")
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun GetDataPreview() {
+    GetData()
+}
